2e873a2860995866b36857549803cee7686c534b
anahostinger <30921896+anahostinger@users.noreply.github.com>
2018-06-07 12:33:31 +0700

    Add files via upload

---
+++ www/assets/apps-investor-180420.js
@@ -0,0 +1,7 @@
+script["apps"] = true;
+function set_login(){ $("body").removeClass("light-gray").addClass("white"); $("#content-app").html(html_login());}
+function set_home(){}
+function set_logout(){}
+function set_profil(){}
+function set_reset_password(){}
+function set_tentang(){}
